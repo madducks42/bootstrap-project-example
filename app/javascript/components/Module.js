2a5912fe7c85bbc1726156abf5f5167dc5ce6f92
Maddox Grey <35937956+madducks42@users.noreply.github.com>
2021-01-21 14:39:28 -0500

    - Fixed Bootstrap JS issues, mostly working now.

--- app/javascript/components/Module.js
+++ app/javascript/components/Module.js
@@ -3,22 +3,25 @@ import React from "react";
 export const Module = () => {
   return (
     <main>
-      <div>
-        <h1>Testing Module</h1>
-      </div>
-      <div className="modal" tabIndex="-1">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title">Modal title</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+        Launch demo modal
+      </button>
+
+      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
             </div>
-            <div className="modal-body">
-              <p>Modal body text goes here.</p>
+            <div class="modal-body">
+              ...
             </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" className="btn btn-primary">Save changes</button>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
             </div>
           </div>
         </div>
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,16 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("jquery")
-require("@rails/ujs").start()
-require("@rails/activestorage").start()
-require("channels")
-require("bootstrap")
+import 'bootstrap'
+import Rails from "@rails/ujs"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
 
-import $ from "jquery"
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  $('[data-toggle="tooltip"]').tooltip()
-  $('[data-toggle="popover"]').popover()
-})
+Rails.start()
+ActiveStorage.start()
